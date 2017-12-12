f48a2a9b6f439864562057d44f67c53c34af4201
Ezio <ballarin@sonoma.edu>
2017-12-12 15:47:53 -0800

    Added header/footer bootstrap elements, and started building the About page.

--- routes/about.js
+++ routes/about.js
@@ -1,13 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
-
 router.get('/about/', function(req, res){
-  res.render('about/about')
+    res.render('about/about')
 });
 
 module.exports = router;
