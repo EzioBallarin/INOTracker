d3165ac2f2d9f4b34cb9bdaafc669f7ad2d5f69e
Ezio <ballarin@sonoma.edu>
2017-12-12 18:26:06 -0800

    Added registration system. Included Bcrypt package for password hashing. Created a new dberror ejs template for general errors.

--- model/register.js
+++ model/register.js
@@ -1,5 +1,19 @@
 var mysql = require('mysql');
 var db = require('./db_connection');
-
 var connection = mysql.createConnection(db.config);
 
+exports.registerNewUser = function(params, callback){
+    var today = new Date();
+    var query = "INSERT INTO " +
+        "p2_users(first_name, last_name, email, password, created, modified)  " +
+        "VALUES (?)";
+    var queryData = [[
+                    params.inot_fname, params.inot_lname,
+                    params.inot_email, params.bcryptedPass,
+                    today, today]];
+    console.log("Inserting...\n");
+    connection.query(query, queryData, function(err, result){
+        callback(err, result);
+    });
+
+};
--- routes/register_routes.js
+++ routes/register_routes.js
@@ -1,10 +1,24 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
 var router = express.Router();
 var register = require('../model/register.js');
 
 router.get('/', function(req, res){
     console.log("registering...");
     res.render('register/register');
+});
+
+router.post('/new', function (req, res) {
+    bcrypt.hash(req.body.inot_pass, 5, function (err, bcryptedPass) {
+        req.body.bcryptedPass = bcryptedPass;
+        register.registerNewUser(req.body, function(err, result) {
+            if (err) {
+                console.log(err);
+                res.render('./dberror', {error: err});
+            } else
+                res.render('./index', {newAccount: true});
+        });
+    })
 });
 
 module.exports = router;
