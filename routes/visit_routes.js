4d89201f33cfc9f51afbd7619d9c0d1ca061ae03
Ezio <ballarin@sonoma.edu>
2017-12-14 21:46:28 -0800

    Added ability to add new visits to a user's account. Added a failure message to the login screen if login failed.

--- routes/visit_routes.js
+++ routes/visit_routes.js
@@ -5,12 +5,18 @@ var visits = require('../model/visits');
 router.get('/', function(req, res){
     if (typeof req.session.user === 'undefined') {
         res.render('visits/visits', {noUser: true, message: "Please login to view your visits"});
+    } else {
+        visits.getUserVisits(req.session.user.user_id, function (err, result) {
+            console.log(err);
+            console.log(result);
+            res.render('visits/visits', {visits: result[0]});
+        });
     }
-    visits.getUserVisits(req.session.user.user_id, function (err, result) {
-        console.log(err);
-        console.log(result);
-        res.render('visits/visits', {visits: result[0]});
-    });
+});
+
+router.post('/new', function (req, res) {
+    console.log(req.body);
+    res.redirect('/visits');
 });
 
 module.exports = router;
