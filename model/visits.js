eae4246cce292a52335b3354ea62fb6fbf1d9d89
Ezio <ballarin@sonoma.edu>
2017-12-14 22:59:22 -0800

    Added ability to delete a visit from a user's account. Changed style of header/footer. Altered order of options in the header.

--- model/visits.js
+++ model/visits.js
@@ -9,3 +9,45 @@ exports.getUserVisits = function(email, callback) {
         callback(err, result);
     });
 };
+
+var getUserID = function(email, callback) {
+    var query = "SELECT user_id FROM p2_users WHERE email=?"
+    connection.query(query, [email], function (err, result) {
+        callback(err, result);
+    });
+};
+
+exports.newUserVisit = function(params, callback) {
+    getUserID(params[0], function (err, result) {
+        if (err)
+            callback(err, result);
+        else {
+            params[0] = result[0].user_id;
+            var query = "INSERT INTO p2_user_visit_location (user_id, store_num, visit_date," +
+                "visit_reason, visit_meal, visit_note)" +
+                " VALUES (?)";
+            if (!params[2]) {
+                params[2] = new Date();
+            }
+            connection.query(query, [params], function (err, result){
+                callback(err, result);
+            });
+        }
+    });
+};
+
+exports.deleteVisit = function (params, callback) {
+
+    getUserID(params.user_id, function (err, result) {
+        if (err)
+            callback(err, result);
+        else {
+            var query = "DELETE FROM p2_user_visit_location WHERE user_id = ?" +
+                " AND visit_id = ? AND store_num = ?";
+            var queryData = [[result[0].user_id], [params.visit_id], [params.store_num]];
+            connection.query(query, queryData, function (err, result) {
+                callback(err, result);
+            });
+        }
+    })
+}
--- routes/visit_routes.js
+++ routes/visit_routes.js
@@ -4,19 +4,49 @@ var visits = require('../model/visits');
 
 router.get('/', function(req, res){
     if (typeof req.session.user === 'undefined') {
-        res.render('visits/visits', {noUser: true, message: "Please login to view your visits"});
+        res.render('visits/visits', {noUser: true, message: "Please login to view your visits", newVisit: false});
     } else {
         visits.getUserVisits(req.session.user.user_id, function (err, result) {
-            console.log(err);
-            console.log(result);
-            res.render('visits/visits', {visits: result[0]});
+            res.render('visits/visits', {visits: result[0], newVisit: false});
         });
     }
 });
 
 router.post('/new', function (req, res) {
-    console.log(req.body);
-    res.redirect('/visits');
+    var visit_data = [
+        req.session.user.user_id,
+        req.body.inot_new_visit_store_num,
+        req.body.inot_new_visit_timestamp,
+        req.body.inot_new_visit_reason,
+        req.body.inot_new_visit_meal,
+        req.body.inot_new_visit_note
+    ];
+    visits.newUserVisit(visit_data, function (err, result) {
+        if (err) {
+            console.log(err);
+            res.render('dberror', {message: err});
+        } else {
+            res.locals.newVisit = true;
+            res.redirect('/visits');
+        }
+    });
+});
+
+router.post('/delete', function (req, res) {
+    var visit_delete_data = {
+        user_id: req.session.user.user_id,
+        visit_id: req.body.inot_visit_num,
+        store_num: req.body.inot_store_num
+    };
+    visits.deleteVisit(visit_delete_data, function (err, result) {
+        if (err) {
+            console.log(err);
+            res.render('dberror', {error: err});
+        } else {
+            res.locals.deletedVisit = true;
+            res.redirect('/visits')
+        }
+    })
 });
 
 module.exports = router;
